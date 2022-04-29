a9c0f4c4a994916ad9a3ee76fbb6f6d6a39e156f
Petebran3 <102988657+Petebran3@users.noreply.github.com>
2022-04-29 14:19:58 -0400

    Add files via upload

---
+++ lo.js
@@ -0,0 +1,24 @@
+// 1.
+const arrowMyFunction = (num1, num2) => {
+    return `${num1} + ${num2}`
+}
+// 1. stops here
+
+
+
+//2.
+
+const arrowAdd100 = (add100) => money + 100
+
+//2. stops here
+
+
+//3.
+const letters = (message, name, p) => {
+    return `${message} ${name} ${p}`
+}
+console.log(letters("hello my name is", "pete", "p"));
+
+//3. stops here
+
+
